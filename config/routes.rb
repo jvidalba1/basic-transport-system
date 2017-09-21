4c29f11505cfc3c3c70f3dc6dffc39a3909a5c27
Mateo Vidal <jvidalba@gmail.com>
2017-09-20 23:29:39 -0500

    Set up Boostrap config and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_scope :passenger do
+    root to: "devise/sessions#new"
+  end
   devise_for :passengers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
