5645a500aa38013ac58857b11f1034d4b526b42f
Mateo Vidal <jvidalba@gmail.com>
2017-09-20 21:43:58 -0500

    Annotate models

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: routes
+#
+#  id           :integer          not null, primary key
+#  passenger_id :integer
+#  vehicle_id   :integer
+#  date         :date
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Route < ApplicationRecord
   belongs_to :passenger
   belongs_to :vehicle
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: vehicles
+#
+#  id            :integer          not null, primary key
+#  license_plate :string
+#  category      :string
+#  color         :string
+#  year          :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Vehicle < ApplicationRecord
 end
